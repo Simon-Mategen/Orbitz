ce7ef98abad42d098a338df5745ddede57456163
Simon-Mategen <62050394+Simon-Mategen@users.noreply.github.com>
2020-03-31 20:27:23 +0200

    Added class to read from API
    Added a class so you can read from the api.

---
+++ src/Controller/Calculators/OrbitCalculator.java
@@ -0,0 +1,5 @@
+package Controller.Calculators;
+
+public class OrbitCalculator
+{
+}
---
+++ src/Controller/Controller.java
@@ -0,0 +1,12 @@
+package Controller;
+
+public class Controller
+{
+    private APIReader reader;
+
+    public Controller()
+    {
+        reader = new APIReader();
+    }
+
+}
