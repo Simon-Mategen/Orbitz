6b2a123ad0c42de0a521fd185f5540db1462a830
Simon-Mategen <62050394+Simon-Mategen@users.noreply.github.com>
2020-04-06 12:42:00 +0200

    Add APIreader

---
+++ src/APIReader.java
@@ -0,0 +1,39 @@
+package Controller;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class APIReader
+{
+    public JSONObject readBodyFromAPI(String body)
+    {
+        HttpClient client = HttpClient.newHttpClient();
+        JSONObject jObject = new JSONObject();
+        try
+        {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://api.le-systeme-solaire.net/rest/bodies/" + body))
+                    .build();
+
+            HttpResponse<String> reply = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String replyContent = reply.body();
+
+            JSONParser parser = new JSONParser();
+
+            jObject = (JSONObject) parser.parse(replyContent);
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("ReadBodyFromAPI " + e.getMessage());
+        }
+
+        return jObject;
+    }
+}
