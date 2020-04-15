586030cfdb0f8359dce1cd76924c09bb4230705b
Manna-Manojlovic <manojlovic.manna@gmail.com>
2020-04-15 12:48:46 +0200

    Edited all my written classes after code review

--- src/View/ImageGalleryPanel.java
+++ src/View/ImageGalleryPanel.java
@@ -8,64 +8,75 @@ import java.io.File;
 import java.io.IOException;
 
 /**
+ * @Author: Manna Manojlovic
+ *
  * Panel for the gallery of images.
  * 4 images of the planet and/or its' moon/moons.
  * 1 enlarged image shows up when user clicks one of the small images.
  *
  */
 
-//TODO 1 large image that changes depending on which small image is selected
-//TODO add listeners - MouseActionEvent?
-
-
 public class ImageGalleryPanel extends JPanel
 {
 
-    private GridLayout layout;
     private JPanel panel;
     private JLabel lblimage1, lblimage2, lblimage3, lblimage4;
 
-    public ImageGalleryPanel() throws IOException
+    public ImageGalleryPanel()
     {
         createPanel();
     }
 
 
-    public void createPanel() throws IOException
+    public void createPanel()
     {
-        layout = new GridLayout(1,4,4,4);
-        panel = new JPanel(layout);
-        panel.setPreferredSize(new Dimension(600,200));
-        panel.setBackground(Color.black);
-        setBackground(Color.black);
+        GridLayout layout = new GridLayout (1,4,4,4);
+
+        panel = new JPanel (layout);
 
         lblimage1 = new JLabel();
         lblimage2 = new JLabel();
         lblimage3 = new JLabel();
         lblimage4 = new JLabel();
 
-        BufferedImage img1 = ImageIO.read(new File("images/image1.jpg"));
-        ImageIcon icon1 = new ImageIcon(img1);
-        lblimage1.setIcon(icon1);
+        BufferedImage img1 = null;
+        BufferedImage img2 = null;
+        BufferedImage img3 = null;
+        BufferedImage img4 = null;
 
-        BufferedImage img2 = ImageIO.read(new File("images/image2.jpg"));
-        ImageIcon icon2 = new ImageIcon(img2);
-        lblimage2.setIcon(icon2);
+        try
+        {
+            img1 = ImageIO.read (new File("images/image1.jpg"));
+            img2 = ImageIO.read (new File ("images/image2.jpg"));
+            img3 = ImageIO.read (new File ("images/image3.jpg"));
+            img4 = ImageIO.read (new File ("images/image4.jpg"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
-        BufferedImage img3 = ImageIO.read(new File("images/image3.jpg"));
-        ImageIcon icon3 = new ImageIcon(img3);
-        lblimage3.setIcon(icon3);
+        ImageIcon icon1 = new ImageIcon (img1);
+        ImageIcon icon2 = new ImageIcon (img2);
+        ImageIcon icon3 = new ImageIcon (img3);
+        ImageIcon icon4 = new ImageIcon (img4);
 
-        BufferedImage img4 = ImageIO.read(new File("images/image4.jpg"));
-        ImageIcon icon4 = new ImageIcon(img4);
-        lblimage4.setIcon(icon4);
+        lblimage1.setIcon (icon1);
+        lblimage2.setIcon (icon2);
+        lblimage3.setIcon (icon3);
+        lblimage4.setIcon (icon4);
 
-        panel.add(lblimage1);
-        panel.add(lblimage2);
-        panel.add(lblimage3);
-        panel.add(lblimage4);
+        panel.setPreferredSize (new Dimension (600,200));
+        panel.setBackground (Color.black);
 
-        add(panel, BorderLayout.EAST);
+        setBackground (Color.black);
+
+        panel.add (lblimage1);
+        panel.add (lblimage2);
+        panel.add (lblimage3);
+        panel.add (lblimage4);
+
+        add (panel, BorderLayout.EAST);
     }
 
 }
--- src/View/MainInfoFrame.java
+++ src/View/MainInfoFrame.java
@@ -1,30 +1,30 @@
 package View;
 
 import javax.swing.*;
-import java.io.IOException;
 
 public class MainInfoFrame extends JFrame
 {
     private MainInfoPanel panel;
 
-    public MainInfoFrame() throws IOException
+    public MainInfoFrame ()
     {
         frame();
     }
 
-    public void frame() throws IOException
+    public void frame ()
     {
-        panel = new MainInfoPanel();
-        setSize(1000, 600);
-        setVisible(true);
-        setContentPane(panel);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new MainInfoPanel ();
+
+        setSize (1000, 600);
+        setVisible (true);
+        setContentPane (panel);
+        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
     }
 
     //TODO JavaFX panel för omloppsbanesystem för planet + månar i miniatyr typ
     //TODO lägg till panelen nånstans - förslagsvis på en MainPanel som sen läggs på denna frame?
 
-    public static void main (String [] args) throws IOException
+    public static void main (String [] args)
     {
         MainInfoFrame iw = new MainInfoFrame();
 
