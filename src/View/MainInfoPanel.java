69682d52f25434c8d416003f61c1b8264107c1b3
Manna-Manojlovic <manojlovic.manna@gmail.com>
2020-04-15 12:19:13 +0200

    Edited MainInfoPanel after code review

--- src/View/MainInfoPanel.java
+++ src/View/MainInfoPanel.java
@@ -7,14 +7,14 @@ import java.awt.*;
 import java.io.IOException;
 
 /**
-        * upper left corner: JavaFX-panel with lunar orbits around planets, in miniature
-        * bottom left corner: textare with general facts about the planet
-        *
-        * upper right corner: image gallery with 4 images in miniature. Underneath is one big picture which changes after a small is clicked
-        * bottom right corner: SWEN THE ALIEN AND HIS FUN FACTS!!!
-        */
-
-//TODO split this MainPanel into 2?
+ * @Author: Manna Manojlovic
+ *
+* upper left corner: JavaFX-panel with lunar orbits around planets, in miniature
+* bottom left corner: textare with general facts about the planet
+*
+* upper right corner: image gallery with 4 images in miniature. Underneath is one big picture which changes after a small is clicked
+* bottom right corner: SWEN THE ALIEN AND HIS FUN FACTS!!!
+*/
 
 public class MainInfoPanel extends JPanel
 {
@@ -31,27 +31,20 @@ public class MainInfoPanel extends JPanel
 
     public void setupPanel()
     {
-        setBackground(Color.black);
-        layout = new BorderLayout();
-        setLayout(layout);
-
         int width = 1000;
         int height = 600;
 
         Border border = this.getBorder();
         Border margin = BorderFactory.createEmptyBorder(12, 12, 12, 12);
+
+        layout = new BorderLayout();
+        setLayout(layout);
+        setBackground(Color.black);
         setPreferredSize(new Dimension(width, height));
         setBorder(new CompoundBorder(border, margin));
 
         add(imgPanel,layout.NORTH);
         add(swenPanel,layout.EAST);
 
-    }
-    public ImageGalleryPanel getImageGalleryPanel()
-    {
-        return imgPanel;
-    }
-    public SwenTheAlien getSwenPanel(){
-        return swenPanel;
     }
 }
